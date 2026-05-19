067c99cec2964a52f886c7d3be8812902016c9d0
kindun <j23422@inc.kisarazu.ac.jp>
2026-05-20 06:40:02 +0900

    feat: 周囲のMineの数を表示
    - tdエレメントを2次元配列で保持した
    - 生成時以外でも取得可能

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import './style.css'
 
-const ROWS:number = 10
-const COLS:number = 10
-const MINES:number = 10
+const ROWS: number = 10
+const COLS: number = 10
+const MINES: number = 10
 
 interface Cell {
   row: number
@@ -13,7 +13,7 @@ interface Cell {
   adjacentMines: number
 }
 
-function placeMines(mines: number): Set<string>{
+function placeMines(mines: number): Set<string> {
   const mineSet = new Set<string>()
   for (let i = 0; i < mines; i++) {
     const r = Math.floor(Math.random() * ROWS)
@@ -28,16 +28,19 @@ type Board = Cell[][];
 const board: Board = []
 const table = document.createElement('table')
 const mineSet: Set<string> = placeMines(MINES)
+const tdGrid: HTMLTableCellElement[][] = []
 
 for (let r = 0; r < ROWS; r++) {
   const tr = document.createElement('tr')
   board[r] = []
+  tdGrid[r] = []
   for (let c = 0; c < COLS; c++) {
     const td = document.createElement('td')
+    tdGrid[r][c] = td
     tr.appendChild(td)
     board[r][c] = {
-      row: r, 
-      col: c, 
+      row: r,
+      col: c,
       isFlagged: false,
       isMine: false,
       isOpen: false,
@@ -51,6 +54,31 @@ for (let r = 0; r < ROWS; r++) {
   }
   table.appendChild(tr)
 }
+
+
+const direction: [number, number][] = [
+  [-1, -1], [-1, 0], [-1, 1],
+  [0, -1], [0, 1],
+  [1, -1], [1, 0], [1, 1]
+]
+
+for (let r = 0; r < ROWS; r++) {
+  for (let c = 0; c < COLS; c++) {
+    if (!board[r][c].isMine) {
+      for (const [dr, dc] of direction) {
+        const nr = r + dr
+        const nc = c + dc
+        if (nr >= 0 && nr < ROWS && nc >= 0 && nc < COLS) {
+          if (board[nr][nc].isMine) {
+            board[r][c].adjacentMines++
+          }
+        }
+        tdGrid[r][c].textContent = String(board[r][c].adjacentMines)
+      }
+    }
+  }
+}
+
 
 document.body.appendChild(table)
 
