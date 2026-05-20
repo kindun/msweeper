efaf8b093a92eb3a3d0d3055469769c772c83de7
kindun <j23422@inc.kisarazu.ac.jp>
2026-05-20 23:31:19 +0900

    feat: スタート処理の実装
    - クリックマス周辺をMine禁止にした
    - 1回のみクリック可能とした

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import './style.css'
 const ROWS: number = 10
 const COLS: number = 10
 const MINES: number = 10
+let isStarted: boolean = true
 
 interface Cell {
   row: number
@@ -13,11 +14,24 @@ interface Cell {
   adjacentMines: number
 }
 
-function placeMines(mines: number): Set<string> {
+const direction: [number, number][] = [
+  [-1, -1], [-1, 0], [-1, 1],
+  [0, -1], [0, 1],
+  [1, -1], [1, 0], [1, 1]
+]
+
+
+function placeMines(mines: number, click_r: number, click_c: number): Set<string> {
   const mineSet = new Set<string>()
-  for (let i = 0; i < mines; i++) {
+  outer: for (let i = 0; i < mines; i++) {
     const r = Math.floor(Math.random() * ROWS)
     const c = Math.floor(Math.random() * COLS)
+    if (r == click_r && c == click_c) continue outer
+    for (const [dr, dc] of direction) {
+      const nr = click_r + dr
+      const nc = click_c + dc
+      if (nr == r && nc == c) continue outer
+    }
     mineSet.add(`${r},${c}`)
   }
   return mineSet
@@ -27,53 +41,60 @@ type Board = Cell[][];
 
 const board: Board = []
 const table = document.createElement('table')
-const mineSet: Set<string> = placeMines(MINES)
 const tdGrid: HTMLTableCellElement[][] = []
 
-for (let r = 0; r < ROWS; r++) {
-  const tr = document.createElement('tr')
-  board[r] = []
-  tdGrid[r] = []
-  for (let c = 0; c < COLS; c++) {
-    const td = document.createElement('td')
-    tdGrid[r][c] = td
-    tr.appendChild(td)
-    board[r][c] = {
-      row: r,
-      col: c,
-      isFlagged: false,
-      isMine: false,
-      isOpen: false,
-      adjacentMines: 0
+function createGrid() {
+  for (let r = 0; r < ROWS; r++) {
+    const tr = document.createElement('tr')
+    board[r] = []
+    tdGrid[r] = []
+    for (let c = 0; c < COLS; c++) {
+      const td = document.createElement('td')
+      tdGrid[r][c] = td
+      tr.appendChild(td)
+      board[r][c] = {
+        row: r,
+        col: c,
+        isFlagged: false,
+        isMine: false,
+        isOpen: false,
+        adjacentMines: 0
+      }
+      //td.addEventListener("click", () => alert(JSON.stringify(board[r][c])))
+      td.addEventListener("click", () => {
+        if (isStarted) {
+          isStarted = false
+          startGame(r, c)
+          return
+        }
+      })
     }
-    if (mineSet.has(`${r},${c}`)) {
-      board[r][c].isMine = true
-      td.style.backgroundColor = `red`
-    }
-    td.addEventListener("click", () => alert(JSON.stringify(board[r][c])))
+    table.appendChild(tr)
   }
-  table.appendChild(tr)
 }
 
+createGrid()
 
-const direction: [number, number][] = [
-  [-1, -1], [-1, 0], [-1, 1],
-  [0, -1], [0, 1],
-  [1, -1], [1, 0], [1, 1]
-]
+function startGame(r: number, c: number) {
+  const mineSet: Set<string> = placeMines(MINES, r, c)
 
-for (let r = 0; r < ROWS; r++) {
-  for (let c = 0; c < COLS; c++) {
-    if (!board[r][c].isMine) {
-      for (const [dr, dc] of direction) {
-        const nr = r + dr
-        const nc = c + dc
-        if (nr >= 0 && nr < ROWS && nc >= 0 && nc < COLS) {
-          if (board[nr][nc].isMine) {
-            board[r][c].adjacentMines++
+  for (let r = 0; r < ROWS; r++) {
+    for (let c = 0; c < COLS; c++) {
+      if (mineSet.has(`${r},${c}`)) {
+        board[r][c].isMine = true
+        tdGrid[r][c].style.backgroundColor = `red`
+      }
+      if (!board[r][c].isMine) {
+        for (const [dr, dc] of direction) {
+          const nr = r + dr
+          const nc = c + dc
+          if (nr >= 0 && nr < ROWS && nc >= 0 && nc < COLS) {
+            if (board[nr][nc].isMine) {
+              board[r][c].adjacentMines++
+            }
           }
+          tdGrid[r][c].textContent = String(board[r][c].adjacentMines)          
         }
-        tdGrid[r][c].textContent = String(board[r][c].adjacentMines)
       }
     }
   }
