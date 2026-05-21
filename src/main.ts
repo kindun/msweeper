b8c619921493d2c19bf195c9e4e3d7b2efca1ece
kindun <j23422@inc.kisarazu.ac.jp>
2026-05-21 11:17:33 +0900

    fix-issue: #1 爆弾個数のバク
    - 原因：Mineの設置と周囲のカウントを同時にしていた
    - placeMinesでMineの設置で解決

--- src/main.ts
+++ src/main.ts
@@ -16,25 +16,27 @@ interface Cell {
 
 const direction: [number, number][] = [
   [-1, -1], [-1, 0], [-1, 1],
-  [0, -1], [0, 1],
+  [0, -1],           [0, 1],
   [1, -1], [1, 0], [1, 1]
 ]
 
 
-function placeMines(mines: number, click_r: number, click_c: number): Set<string> {
+function placeMines(mines: number, click_r: number, click_c: number) {
   const mineSet = new Set<string>()
   outer: for (let i = 0; i < mines; i++) {
     const r = Math.floor(Math.random() * ROWS)
     const c = Math.floor(Math.random() * COLS)
     if (r == click_r && c == click_c) continue outer
     for (const [dr, dc] of direction) {
+      /* クリックしたセルの周囲はmineにしない*/
       const nr = click_r + dr
       const nc = click_c + dc
       if (nr == r && nc == c) continue outer
     }
     mineSet.add(`${r},${c}`)
+    board[r][c].isMine = true
+    tdGrid[r][c].style.backgroundColor = `red`
   }
-  return mineSet
 }
 
 type Board = Cell[][];
@@ -60,7 +62,7 @@ function createGrid() {
         isOpen: false,
         adjacentMines: 0
       }
-      //td.addEventListener("click", () => alert(JSON.stringify(board[r][c])))
+      td.addEventListener("click", () => alert(JSON.stringify(board[r][c])))
       td.addEventListener("click", () => {
         if (isStarted) {
           isStarted = false
@@ -76,19 +78,16 @@ function createGrid() {
 createGrid()
 
 function startGame(r: number, c: number) {
-  const mineSet: Set<string> = placeMines(MINES, r, c)
+  placeMines(MINES, r, c)
 
+  /* Mineをセットする */
   for (let r = 0; r < ROWS; r++) {
     for (let c = 0; c < COLS; c++) {
-      if (mineSet.has(`${r},${c}`)) {
-        board[r][c].isMine = true
-        tdGrid[r][c].style.backgroundColor = `red`
-      }
       if (!board[r][c].isMine) {
         for (const [dr, dc] of direction) {
           const nr = r + dr
           const nc = c + dc
-          if (nr >= 0 && nr < ROWS && nc >= 0 && nc < COLS) {
+          if (0 <= nr && nr < ROWS && 0 <= nc && nc < COLS) {
             if (board[nr][nc].isMine) {
               board[r][c].adjacentMines++
             }
